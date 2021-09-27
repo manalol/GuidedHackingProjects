921293214ab483e3cb0d6e5278e66bfc3d469e4b
mana <manaainquires@gmail.com>
2021-09-27 17:00:49 -0500

    added mem

--- HackAnyGame/HackAnyGame.cpp
+++ HackAnyGame/HackAnyGame.cpp
@@ -1,38 +1,42 @@
 
-#include "proc.h"
+	#include "proc.h"
 
-int main()
-{
-	
-	// Get process ID
-	DWORD processID = GetProcId(L"ac_client.exe");
-	std::cout << "Process ID: " << processID << std::endl;
-
-	// Get module base address
-	uintptr_t modBaseAddress = GetModuleBaseAddress(processID, L"ac_client.exe");
-	std::cout << "Module base address: 0x" << std::hex << modBaseAddress << std::endl;
-
-	// Get a handle to the process
+	int main()
+	{
+	// Initalize process variables, addresses and toggles.
 	HANDLE hProcess = 0;
-	hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, processID);
 
-	// Resolve (find) base address of pointer chain
-	uintptr_t dynamicBasePointerAddress = modBaseAddress + 0x10F4F4;
+	uintptr_t moduleBase, localPlayerPtr, healthAddr;
+	bool bHealth, bAmmo, bRecoil;
 
-	std::cout << "Base pointer address: 0x" << dynamicBasePointerAddress << std::endl;
-	// Resolve base address of ammo pointer chain
-	uintptr_t ammoAddress = FindDMAAddy(hProcess, dynamicBasePointerAddress, std::vector<unsigned int>{0x374, 0x14, 0x0});
-	std::cout << "Ammo address: 0x" << ammoAddress << std::endl;
+	// Set our buffer for the health & get process ID
+	constexpr int newValue{ 1337 };
+	DWORD processID = GetProcId(L"ac_client.exe");
 
-	// Read ammo, edit it then read it again
-	int currAmmo;
-	int newAmmo{ 9999 };
+	while (true)
+	{
+		if (processID)
+		{
+			hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, processID);
+			moduleBase = GetModuleBaseAddress(processID, L"ac_client.exe");
+			localPlayerPtr = moduleBase + 0x10f4f4;
 
-	ReadProcessMemory(hProcess, (BYTE*)ammoAddress, &currAmmo, sizeof(currAmmo), 0);
-	std::cout << "Current ammo amount: " << std::dec << currAmmo << std::endl;
-	WriteProcessMemory(hProcess, (BYTE*)ammoAddress, &newAmmo, sizeof(newAmmo), 0);
-	std::cout << "Changing ammo..." << std::endl;
-	ReadProcessMemory(hProcess, (BYTE*)ammoAddress, &currAmmo, sizeof(currAmmo), 0);
-	std::cout << "Current ammo amount: " << currAmmo << std::endl;
+			healthAddr = FindDMAAddy(hProcess, localPlayerPtr, { 0xf8 });
+			break;
+		}
+		else
+		{
+			// Keep trying to get the process ID
+			std::cout << "Process not found, press enter to try again";
+			getchar();
+			processID = GetProcId(L"ac_client.exe");
+		}
+	}
+	
+	
+	DWORD dwExit{ 0 };
+	while (GetExitCodeProcess(hProcess, &dwExit) && dwExit == STILL_ACTIVE)
+	{
 
-}
+	}
+	
